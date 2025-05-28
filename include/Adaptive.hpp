71681f361e64132acc0233b2a778d94f2906b073
zavadsky <zava@ukr.net>
2025-05-28 22:36:13 +0300

    Add files via upload

--- include/Adaptive.hpp
+++ include/Adaptive.hpp
@@ -5,7 +5,6 @@
 #include <map>
 #include <set>
 #include <deque>
-//#include "BitIoStream.hpp"
 #include "WordBasedText.h"
 
 using namespace std;
@@ -35,55 +34,6 @@ protected:
         };
 };
 
-// Our method
-class AdaptiveN : public BackwardEncoder {
-protected:
-    vector<int> m;
-    vector<int> rev;
-    vector<int> ind;
-    vector<int> f,f1;
-    UniversalCode* C;
-    vector<uint8_t> bestDigitsSz;
-    int j;
-    const static int max_var_digit=12,init_j=26;
-    void update1(int);
-    void update_code(uint32_t);
-    void update_decode(uint32_t);
-    void update_code_char(uint32_t);
-    void update_decode_char(uint32_t);
-    void precalcBestBlocks();
-    void BCS(uint8_t[], uint32_t, uint64_t, uint64_t, uint64_t, uint64_t);
-    const static int max_digit=40;
-    int bestFullBitsSz = 10000000,bestBlocksNum;
-    static const int sigma = 256;
-public:
-    int mc=0;
-    uint32_t* PSum;
-    uint64_t  getSum(uint32_t, uint32_t);
-    double rate=5;
-    AdaptiveN(WordBasedText*,UniversalCode*);
-    AdaptiveN(UniversalCode*,int);
-    AdaptiveN(WordBasedText*,UniversalCode*,vector<uint32_t>);
-    double encode();
-    double encode1();
-    double encode2();
-    double encode2_char();
-    void print_code();
-    void update();
-    int decode();
-    int decode1();
-    int decode2();
-    ~AdaptiveN(){delete[] outChar;};
-};
-
-class AdaptiveNDecode: public AdaptiveN {
-private:
-    int n;
-public:
-    int decode2_char();
-    AdaptiveNDecode(int,UniversalCode*);
-    ~AdaptiveNDecode(){delete[] outChar;};
-};
 
 typedef struct {
     unsigned up,      // next node up the tree
@@ -131,7 +81,9 @@ public:
 class VitterDecode: public Vitter {
 public:
     VitterDecode(int);
-    ~VitterDecode(){delete[] outChar;};;
+    ~VitterDecode(){
+        delete[] outChar;
+    };
     unsigned huff_decode (HCoder*);
     void decode_char();
 private:
@@ -139,38 +91,6 @@ private:
     unsigned huff_readid (HCoder*);
     uint32_t outPos,inPos;
 };
-
-// Modified Vitter algorithm
-class VitterM : public BackwardEncoder {
-private:
-    FILE *In, *Out;
-    unsigned char ArcBit = 0;
-    int ArcChar = 0;
-    HCoder *huff;
-    int len=0,mc=0;
-    const int size = (1<<24);
-    void update(string);
-    HCoder *huff_init (unsigned size, unsigned root);
-    void arc_put1 (unsigned bit);
-    unsigned arc_get1 ();
-    unsigned huff_split (HCoder *, unsigned);
-    unsigned huff_leader (HCoder *, unsigned);
-    unsigned huff_slide (HCoder *, unsigned);
-    void huff_increment (HCoder *, unsigned);
-    void huff_sendid (HCoder *, unsigned);
-    void huff_encode (HCoder *, unsigned);
-    unsigned huff_decode (HCoder *);
-    unsigned huff_readid (HCoder*);
-    unsigned char *buffer;
-public:
-    VitterM(WordBasedText*);
-    double encode();
-    unsigned decode ();
-    int serialize(uint8_t*);
-    void load(uint8_t*,int);
-    ~VitterM(){delete huff;};
-};
-
 
 class Gagie : public BackwardEncoder {
 private:
@@ -203,10 +123,7 @@ public:
     virtual void decode_char1();
     double entropy();
     ~GagieDecode(){
-        delete[] outChar;
-     /*   delete[] freqs;
-        delete[] codeStream;
-        delete C;*/
+        //delete[] outChar;
     };
 protected:
     uint32_t outPos;
@@ -218,10 +135,8 @@ private:
     CHuffman* C;
 public:
     CanonicalDecode(int a,int b):GagieDecode(a,b){};
-    //void decode_char();
     void decode_char1();
     void decode_char();
-    //double entropy();
     ~CanonicalDecode(){};
 };
 
